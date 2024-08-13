6c0dc66d8d3a8b7fc7ca02d795d467ee0a316aea
Marcell Wendling <mwsa.pe@gmail.com>
2024-08-13 12:03:48 -0300

    Terceiro Commit

--- AppListaCurso/app/build.gradle.kts
+++ AppListaCurso/app/build.gradle.kts
@@ -37,7 +37,4 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
