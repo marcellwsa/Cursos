330c9c8dd5c6e0e4168dea52ada76fa14c67caa4
Marcell Wendling <mwsa.pe@gmail.com>
2024-08-13 20:24:49 -0300

    Arquitetura MVC

--- AppListaCurso/app/src/main/java/devandroid/marcell/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/marcell/applistacurso/view/MainActivity.java
@@ -1,4 +1,4 @@
-package devandroid.marcell.applistacurso;
+package devandroid.marcell.applistacurso.view;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import devandroid.marcell.applistacurso.R;
 
 public class MainActivity extends AppCompatActivity {
     //teste
