a7f765878e29dc2b63d01436f03332f5756c3477
Marcell Wendling <mwsa.pe@gmail.com>
2024-08-17 20:01:45 -0300

    Inicial do GasEta. Clonagem.

--- AppGasEta/app/build.gradle.kts
+++ AppGasEta/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "devandroid.marcell.appgaseta"
+    namespace = "devandroid.marcell.appgaseta1"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "devandroid.marcell.appgaseta"
+        applicationId = "devandroid.marcell.appgaseta1"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -37,7 +37,4 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
